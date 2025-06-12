458fe4dd441fde08e9449a7b6f99b6ba6da0d425
Fernando <100nandoo@gmail.com>
2025-06-12 20:31:03 +0800

    remove daisy ui tailwind plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,10 @@
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import daisyui from 'daisyui';
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	daisyui: {
-		themes: ['sunset', 'light']
-	},
-	theme: {
-		extend: {}
-	},
 
-	plugins: [typography, forms, containerQueries, daisyui]
+	plugins: [typography, forms, containerQueries]
 } satisfies Config;
